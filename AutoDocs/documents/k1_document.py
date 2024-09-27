afcb4cd8a4db0882cec3f471655d2fe80852ec3f
ppark271 <ppark@gpes.com>
2024-09-27 09:09:59 -0400

    opens k1_template relative to users computer, no longer hardcoded

--- AutoDocs/documents/k1_document.py
+++ AutoDocs/documents/k1_document.py
@@ -1,7 +1,8 @@
 from documents.utils import *
 
 def create_k1_document_pdf(fund_name, legal_name, output_pdf_path):
-    input_pdf = r"C:\Users\alexander\OneDrive - GP Fund Solutions, LLC\GPES File Engine\documents\k1_template2.pdf"
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    input_pdf = __location__ + "\k1_template.pdf"
     texts_with_positions = [
         (fund_name + " 1456 Sierra Ridge Drive Fresno CA 93711", (40, 580)),
         (legal_name + " 8973 Elliott Stream South Shawnchester CA 97695", (40, 480)),
